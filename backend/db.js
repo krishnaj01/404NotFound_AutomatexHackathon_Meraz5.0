1ba9f97523423d1bce3a19c41236cac62070582d
krishnaj01 <krishna.jhanwar2005@gmail.com>
2024-11-09 15:01:32 +0530

    editing card.js

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAsGrmq7ATvXP4QS_dCPukNUeZSEJ4SdUo",
+    authDomain: "lost-found-iitbhilai-a099c.firebaseapp.com",
+    projectId: "lost-found-iitbhilai-a099c",
+    storageBucket: "lost-found-iitbhilai-a099c.firebasestorage.app",
+    messagingSenderId: "169751245523",
+    appId: "1:169751245523:web:37483871e09f06a2dd1a26"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+module.exports = app;
