0eaba7940c8f2fb5652eac92900f535c73765132
Sidd-247 <siddharthjayprakashrai@gmail.com>
2024-11-09 22:23:27 +0530

    Responsive Navbar

--- frontend/lost-found/src/App.jsx
+++ frontend/lost-found/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 // import './App.css'
 import Landing from './components/custom/Landing'
 import Navbar from './components/HomePage/Navbar'
+import LandingHeader from './components/custom/LandingHeader'
 
 function App() {
 
--- frontend/lost-found/src/components/custom/LandingHeader.jsx
+++ frontend/lost-found/src/components/custom/LandingHeader.jsx
@@ -53,90 +53,151 @@ const LandingHeader = () => {
   };
 
   return (
-    <div>
-      {!user ? (
-        <div className="p-3 shadow-sm flex justify-between items-center px-5">
-          <a href="/">
-            <div className="flex gap-3 items-center cursor-pointer">
-              <img src="/vite.svg" className="h-16" alt="logo" />
-              <h2 className="font-bold text-3xl font-serif">Lost-Found</h2>
+    <>
+      {/* Div 1: Visible on small screens, hidden on medium and larger screens */}
+      <div className="hidden md:block">
+        {!user ? (
+          <div className="p-3 shadow-sm flex justify-between items-center px-5">
+            <a href="/">
+              <div className="flex gap-3 items-center cursor-pointer">
+                <img src="/vite.svg" className="h-16" alt="logo" />
+                <h2 className="font-bold text-3xl font-serif">Lost-Found</h2>
+              </div>
+            </a>
+            <Button onClick={() => setOpenDialog(true)}>Sign In</Button>
+          </div>
+        ) : (
+          <div className="flex items-center justify-between h-16 sm:h-[10vh] px-4 sm:px-8 shadow-sm">
+            <div className="flex space-x-40 w-full sm:w-[40%] justify-center">
+              <Buttons buttonText="Home" />
+              <Buttons buttonText="Lost" />
             </div>
-          </a>
-          <Button onClick={() => setOpenDialog(true)}>Sign In</Button>
-        </div>
-      ) : (
-        <div className="flex items-center justify-between h-16 sm:h-[10vh] px-4 sm:px-8 shadow-sm">
-          <div className="flex space-x-40 w-full sm:w-[40%] justify-center">
-            <Buttons buttonText="Home" />
-            <Buttons buttonText="Lost" />
-          </div>
 
-          <div className="flex justify-center w-[15%] sm:w-[10%]">
-            <img src="/logo.png" alt="logo" className="h-10 sm:h-10" />
-          </div>
+            <div className="flex justify-center w-[15%] sm:w-[10%]">
+              <img src="/logo.png" alt="logo" className="h-10 sm:h-10" />
+            </div>
 
-          <div className="flex space-x-40 w-full sm:w-[40%] justify-center">
-            <Buttons buttonText="Found" />
-            <Popover>
-              <PopoverTrigger>
-                <div>
-                  <div className="flex gap-5 items-center">
-                    <Buttons buttonText="Profile" />
-                    {user.picture ? (
-                      <img
-                        src={user.picture}
-                        alt="profile"
-                        className="h-[35px] w-[35px] rounded-full"
-                      />
-                    ) : (
-                      <img
-                        src="/vite.svg"
-                        alt="profile"
-                        className="h-[35px] w-[35px] rounded-full"
-                      ></img>
-                    )}
+            <div className="flex space-x-40 w-full sm:w-[40%] justify-center">
+              <Buttons buttonText="Found" />
+              <Popover>
+                <PopoverTrigger>
+                  <div>
+                    <div className="flex gap-5 items-center">
+                      <Buttons buttonText="Profile" />
+                      {user.picture ? (
+                        <img
+                          src={user.picture}
+                          alt="profile"
+                          className="h-[35px] w-[35px] rounded-full"
+                        />
+                      ) : (
+                        <img
+                          src="/vite.svg"
+                          alt="profile"
+                          className="h-[35px] w-[35px] rounded-full"
+                        />
+                      )}
+                    </div>
                   </div>
-                </div>
-              </PopoverTrigger>
-              <PopoverContent>
-                <div>
-                  <h2 className="font-bold text-xl mt-2">{user.name}</h2>
-                  <p className="text-sm text-gray-500 mb-4">{user.email}</p>
-                  <div
-                    onClick={() => {
-                      googleLogout();
-                      localStorage.clear();
-                      window.location.replace("/");
-                    }}
-                  >
-                    <Button className="w-full">Logout</Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <div>
+                    <h2 className="font-bold text-xl mt-2">{user.name}</h2>
+                    <p className="text-sm text-gray-500 mb-4">{user.email}</p>
+                    <div
+                      onClick={() => {
+                        googleLogout();
+                        localStorage.clear();
+                        window.location.replace("/");
+                      }}
+                    >
+                      <Button className="w-full">Logout</Button>
+                    </div>
                   </div>
-                </div>
-              </PopoverContent>
-            </Popover>
+                </PopoverContent>
+              </Popover>
+            </div>
           </div>
-        </div>
-      )}
+        )}
 
-      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogDescription className="text-center">
-              <img src="/vite.svg" alt="logo" className="mx-auto h-16" />
-              <h2 className="font-bold text-lg mt-7">Sign In with Google</h2>
-              <p>Sign in to the app with Google authentication securely</p>
-              <Button
-                className="w-full mt-5 flex gap-4 items-center"
-                onClick={login}
+        <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogDescription className="text-center">
+                <img src="/vite.svg" alt="logo" className="mx-auto h-16" />
+                <h2 className="font-bold text-lg mt-7">Sign In with Google</h2>
+                <p>Sign in to the app with Google authentication securely</p>
+                <Button
+                  className="w-full mt-5 flex gap-4 items-center"
+                  onClick={login}
+                >
+                  <FcGoogle style={{ height: "28px", width: "28px" }} />
+                  Sign In with Google
+                </Button>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+
+      {/* Div 2: Hidden on small screens, visible on medium and larger screens */}
+      <div className="flex items-center justify-between h-16 px-4 sm:px-8 md:hidden">
+        <div className="flex justify-center w-[15%] sm:w-[10%]">
+          <img src="/logo.png" alt="logo" className="h-10 sm:h-10" />
+        </div>
+
+        <Popover>
+          <PopoverTrigger>
+            <div>
+              <div className="flex gap-5 items-center">
+                <Buttons buttonText="Profile" />
+                {user.picture ? (
+                  <img
+                    src={user.picture}
+                    alt="profile"
+                    className="h-[35px] w-[35px] rounded-full"
+                  />
+                ) : (
+                  <img
+                    src="/vite.svg"
+                    alt="profile"
+                    className="h-[35px] w-[35px] rounded-full"
+                  />
+                )}
+              </div>
+            </div>
+          </PopoverTrigger>
+          <PopoverContent>
+            <div>
+              <h2 className="font-bold text-xl mt-2">{user.name}</h2>
+              <p className="text-sm text-gray-500 mb-4">{user.email}</p>
+              <div className="hover:bg-purple-300 bg-gray-100 text-center p-2 rounded-md cursor-pointer transition-colors duration-200">
+                Home
+              </div>
+              <div className="hover:bg-purple-300 bg-gray-100 text-center p-2 rounded-md cursor-pointer transition-colors duration-200">
+                Lost
+              </div>
+              <div className="hover:bg-purple-300 bg-gray-100 text-center p-2 rounded-md cursor-pointer transition-colors duration-200">
+                Found
+              </div>
+              <div className="hover:bg-purple-300 bg-gray-100 text-center p-2 rounded-md cursor-pointer transition-colors duration-200">
+                Profile
+              </div>
+              <div
+                onClick={() => {
+                  googleLogout();
+                  localStorage.clear();
+                  window.location.replace("/");
+                }}
               >
-                <FcGoogle style={{ height: "28px", width: "28px" }} />
-                Sign In with Google
-              </Button>
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
-    </div>
+                <Button className="w-full">Logout</Button>
+              </div>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </>
+
   );
 };
 
