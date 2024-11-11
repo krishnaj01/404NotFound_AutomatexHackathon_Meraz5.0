91d0d753560d68bd70a1ec6de0980713fec8bd0d
Slok9931 <148342312+Slok9931@users.noreply.github.com>
2024-11-11 15:48:22 +0530

    firebase connected

--- frontend/lost-found/src/App.jsx
+++ frontend/lost-found/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 // import './App.css'
 import Landing from './components/custom/Landing'
 import ProfileForm from './components/custom/ProfileForm'
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import ItemCard from './components/custom/test';
 
 
