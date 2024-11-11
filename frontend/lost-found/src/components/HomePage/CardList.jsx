8fb811484d4a829db88ac943b7e9031c9a13c0ce
Sidd-247 <siddharthjayprakashrai@gmail.com>
2024-11-11 17:43:55 +0530

    Dashboard made

---
+++ frontend/lost-found/src/components/HomePage/CardList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ItemCards from '../custom/ItemCards'
+
+const CardList = () => {
+    return (
+        // <div>CardList</div>
+        <ItemCards />
+    )
+}
+
+export default CardList
--- frontend/lost-found/src/components/HomePage/Home.jsx
+++ frontend/lost-found/src/components/HomePage/Home.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import CardList from './CardList';
 
 const Home = () => {
   return (
-    <div>
-      Hii
-    </div>
+    <>
+      <div className="w-full h-1/2 ">
+        <img
+          src="./campusimage.png" // replace with your actual background image path
+          alt="Building"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <div className='p-4 mb-8'>
+        <div className="flex justify-center space-x-[50vw] items-center w-full p-4 mb-8">
+          <div className="text-lg font-semibold text-purple-600">LOST ITEMS</div>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-1/3 p-3 rounded-lg border border-purple-300 focus:outline-none focus:ring-2 focus:ring-purple-600"
+          />
+        </div>
+        <CardList />
+      </div>
+
+      <div className='p-4 mb-8'>
+        <div className="flex justify-center space-x-[50vw] items-center w-full p-4 mb-8">
+          <div className="text-lg font-semibold text-purple-600">FOUND ITEMS</div>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-1/3 p-3 rounded-lg border border-purple-300 focus:outline-none focus:ring-2 focus:ring-purple-600"
+          />
+        </div>
+        <CardList />
+      </div>
+
+
+
+
+    </>
   )
 }
 
